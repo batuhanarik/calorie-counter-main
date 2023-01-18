cd47a4068f7fadc1344fb4f22d896caabc6e61bb
Batuhan ARIK <batuhanarik123@gmail.com>
2023-01-19 00:16:16 +0300

    asdasd

---
+++ src/meals.ts
@@ -0,0 +1,20 @@
+const meals = [
+  {
+    id: "kahvalti",
+    name: "kahvaltı",
+  },
+  {
+    id: "oglenyemegi",
+    name: "Öğle Yemeği",
+  },
+  {
+    id: "aksamyemegi",
+    name: "dinner",
+  },
+  {
+    id: "atistirmalik",
+    name: "snacks",
+  },
+];
+
+export default meals;
